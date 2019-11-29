2e9f1325ab98ac4ee8325d7934befe51ea019e78
schio <ghsehr1@gmail.com>
2019-11-30 07:35:36 +0900

    modi cnn_sample

--- Fundus/model.py
+++ Fundus/model.py
@@ -72,19 +72,19 @@ def cnn_sample(in_shape, num_classes=4):    # Example CNN
     model.add(Conv2D(filters=16, kernel_size=(5, 5), padding='same', input_shape=in_shape))
     model.add(BatchNormalization(axis=-1))
     model.add(ReLU())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Conv2D(filters=32, kernel_size=(5, 5), padding='same'))
     model.add(BatchNormalization(axis=-1))
     model.add(ReLU())
-    model.add(ZeroPadding2D(padding=((0, 0), (0, 1))))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Conv2D(filters=64, kernel_size=5, padding='same'))
+    model.add(BatchNormalization(axis=-1))
     model.add(ReLU())
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    # model.add(Dropout(0.9))
 
-    model.add(Conv2D(filters=128, kernel_size=6, padding='same'))
+    model.add(Conv2D(filters=128, kernel_size=5, padding='same'))
     model.add(BatchNormalization(axis=-1))
     model.add(ReLU())
     model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -92,10 +92,12 @@ def cnn_sample(in_shape, num_classes=4):    # Example CNN
     model.add(Conv2D(filters=256, kernel_size=5, padding='same'))
     model.add(BatchNormalization(axis=-1))
     model.add((ReLU()))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Flatten())
     model.add(Dense(100, activation='relu'))
     model.add(Dense(num_classes, activation='softmax'))
 
     return model
+
     
