775b1fef19ad80a5e73c8905c6cc4fe64c519e59
Jungwoo <westhyena@gmail.com>
2019-11-30 02:46:43 +0900

    resnet, densenet ensemble

--- Fundus/ensemble.py
+++ Fundus/ensemble.py
@@ -23,7 +23,7 @@ import keras.backend.tensorflow_backend as K
 import nsml
 from nsml.constants import DATASET_PATH, GPU_NUM
 
-from model import cnn_sample, inception_v3, efficientnet
+from model import cnn_sample, inception_v3, efficientnet, resnext_50, densenet_121
 from dataprocessing import image_preprocessing, dataset_loader
 
 
@@ -33,18 +33,24 @@ RESCALE = True
 
 
 def bind_model(inception_model, inception_ratio,
-               efficient_model, efficient_ratio
+               efficient_model, efficient_ratio,
+               resnet_model, resnet_ratio,
+               densenet_model, densenet_ratio
                ):
     def save(dir_name):
         os.makedirs(dir_name, exist_ok=True)
         inception_model.save_weights(os.path.join(dir_name, 'inception_model'))
         efficient_model.save_weights(os.path.join(dir_name, 'efficient_model'))
+        resnet_model.save_weights(os.path.join(dir_name, 'resnet_model'))
+        densenet_model.save_weights(os.path.join(dir_name, 'densenet_model'))
         # model.save_weights(file_path,'model')
         print('model saved!')
 
     def load(dir_name):
         inception_model.load_weights(os.path.join(dir_name, 'inception_model'))
         efficient_model.load_weights(os.path.join(dir_name, 'efficient_model'))
+        resnet_model.load_weights(os.path.join(dir_name, 'resnet_model'))
+        densenet_model.load_weights(os.path.join(dir_name, 'densenet_model'))
         print('model loaded!')
 
     def infer(data, rescale=RESCALE, resize_factor=RESIZE):  ## test mode
@@ -57,8 +63,11 @@ def bind_model(inception_model, inception_ratio,
 
         inception_pred = inception_model.predict(X)
         efficient_pred = efficient_model.predict(X)
+        resnet_pred = resnet_model.predict(X)
+        densenet_pred = densenet_model.predict(X)
 
         pred = (inception_pred * inception_ratio + efficient_pred * efficient_ratio)
+        pred += (resnet_pred * resnet_ratio + densenet_pred * densenet_ratio)
         pred = np.argmax(pred, axis=1)
         print('Prediction done!\n Saving the result...')
         return pred
@@ -80,6 +89,14 @@ if __name__ == '__main__':
     args.add_argument('--efficient', type=str, default='')
     args.add_argument('--efficient_ckpt', type=str, default='')
     args.add_argument('--efficient_ratio', type=float, default=1.)
+
+    args.add_argument('--resnet', type=str, default='')
+    args.add_argument('--resnet_ckpt', type=str, default='')
+    args.add_argument('--resnet_ratio', type=float, default=1.)
+
+    args.add_argument('--densenet', type=str, default='')
+    args.add_argument('--densenet_ckpt', type=str, default='')
+    args.add_argument('--densenet_ratio', type=float, default=1.)
 
     # DONOTCHANGE: They are reserved for nsml
     args.add_argument('--mode', type=str, default='train', help='submit일때 해당값이 test로 설정됩니다.')
@@ -103,11 +120,15 @@ if __name__ == '__main__':
 
     inception_model = inception_v3(input_shape, num_classes)
     efficient_model = efficientnet(input_shape, num_classes)
+    resnet_model = resnext_50(input_shape, num_classes)
+    densenet_model = densenet_121(input_shape, num_classes)
     
     learning_rate = 1e-4
     sgd = optimizers.SGD(lr=learning_rate, momentum=0.9, nesterov=True)
     inception_model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['categorical_accuracy'])
     efficient_model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['categorical_accuracy'])
+    resnet_model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['categorical_accuracy'])
+    densenet_model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['categorical_accuracy'])
 
     def inception_load(dir_name):
         inception_model.load_weights(os.path.join(dir_name, 'model'))
@@ -116,12 +137,24 @@ if __name__ == '__main__':
     def efficient_load(dir_name):
         efficient_model.load_weights(os.path.join(dir_name, 'model'))
         print('efficient loaded')
+
+    def resnet_load(dir_name):
+        resnet_model.load_weights(os.path.join(dir_name, 'model'))
+        print('resnet loaded')
+
+    def densenet_load(dir_name):
+        densenet_model.load_weights(os.path.join(dir_name, 'model'))
+        print('densenet loaded')
     
     nsml.load(checkpoint=config.inception_ckpt, load_fn=inception_load, session=config.inception)
     nsml.load(checkpoint=config.efficient_ckpt, load_fn=efficient_load, session=config.efficient)
+    nsml.load(checkpoint=config.resnet_ckpt, load_fn=resnet_load, session=config.resnet)
+    nsml.load(checkpoint=config.densenet_ckpt, load_fn=densenet_load, session=config.densenet)
     
     bind_model(inception_model, config.inception_ratio,
-               efficient_model, config.efficient_ratio)
+               efficient_model, config.efficient_ratio,
+               resnet_model, config.resnet_ratio,
+               densenet_model, config.densenet_ratio)
     if config.pause:  ## test mode일 때
         print('Inferring Start...')
         nsml.paused(scope=locals())
@@ -177,8 +210,11 @@ if __name__ == '__main__':
 
         inception_pred = inception_model.predict(X_val)
         efficient_pred = efficient_model.predict(X_val)
+        resnet_pred = resnet_model.predict(X_val)
+        densenet_pred = densenet_model.predict(X_val)
 
         pred = (inception_pred * config.inception_ratio + efficient_pred * config.efficient_ratio)
+        pred += (resnet_pred * config.resnet_ratio + densenet_pred * config.densenet_ratio)
         pred = np.argmax(pred, axis=1)
         y_true = np.argmax(Y_val, axis=1)
         
